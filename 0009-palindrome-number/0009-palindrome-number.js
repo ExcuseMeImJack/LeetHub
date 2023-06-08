e8dcf0891787c15d8543d0af1ed025eee5c443ca
Jack Roybal <118242834+ExcuseMeImJack@users.noreply.github.com>
2023-06-08 08:36:36 -0700

    Time: 372 ms (5.02%), Space: 65.2 MB (5.00%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -5,19 +5,9 @@
 var isPalindrome = function(x) {
     const xStr = x.toString();
     
-    console.log('Check #1')
-    if(xStr.includes('-')) return false
-    console.log('Check #2')
-    if(xStr.length === 2 && xStr[0] === xStr[1]) return true
-    console.log('Check #3')
-    if(xStr.length === 2 && xStr[0] !== xStr[1]) return false
-    else if(xStr.length !== 2){
-        console.log('For Loop Check')
-        for(let i = 0; i < xStr.length/2; i++){
+for(let i = 0; i < xStr.length/2; i++){
             console.log(i)
             if(xStr[i] !== xStr[xStr.length - 1 - i]) return false
         }
-    }
-console.log('Finalizing no case')
 return true
 };
