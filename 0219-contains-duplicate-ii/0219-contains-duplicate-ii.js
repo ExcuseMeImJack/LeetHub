26d5648bbb8a51e02b47270ce25c087d37ccc28d
Jack Roybal <118242834+ExcuseMeImJack@users.noreply.github.com>
2023-06-29 10:46:44 -0700

    Time: 6133 ms (8.29%), Space: 50.4 MB (87.86%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    // Return (true) if there are two DISTINCT INDICES i and j in the array that nums[i] == nums[j] and math.abs(i - j) <= k
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i + 1; j < nums.length; j++){
+            if(nums[i] == nums[j] && Math.abs(i - j) <= k) return true;
+        }
+    }
+    return false;
+};
