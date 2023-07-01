fc2f4cd11017d5f806bcff3a661ea8bf58e682f8
Jack Roybal <118242834+ExcuseMeImJack@users.noreply.github.com>
2023-07-01 11:12:23 -0700

    Time: 91 ms (70.98%), Space: 55.2 MB (30.19%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -4,8 +4,5 @@
  */
 var containsDuplicate = function(nums) {
     const uniqueNums = new Set(nums);
-    console.log(uniqueNums.size)
-    console.log(nums.length)
-    if(uniqueNums.size === nums.length) return false;
-    else if(uniqueNums.size !== nums.length) return true;
+    return uniqueNums.size !== nums.length
 };
