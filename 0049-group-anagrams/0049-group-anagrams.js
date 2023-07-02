6f413e107e04d1daacb9e9544a1e52db8144bfce
Jack Roybal <118242834+ExcuseMeImJack@users.noreply.github.com>
2023-07-02 11:11:50 -0700

    Time: 129 ms (64.88%), Space: 51.8 MB (98.35%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -5,17 +5,21 @@
 var groupAnagrams = function (strs) {
   const res = [];
   const obj = {};
-
+    // console.log('Input: ', strs)
     strs.forEach((word) => {
         // Turn the current word into a string and sort the characters then join it back into a string.
+        // console.log('Current Word: ', word)
         const sortedStr = word.split("").sort().join(""); 
         // If the sorted string is in the object;
-        if (sortedStr in obj) {
+        // console.log('Sorted Word: ', sortedStr)
+        if (sortedStr in obj) { // Checks to see if the sorted string is already an anagram.
             // Push the word into the obj at the sorted string.
             obj[sortedStr].push(word);
+            // console.log('Object TRUE: ', obj)
         } else {
             // Set the word into the obj at the sorted string.
             obj[sortedStr] = [word];
+            // console.log('Object FALSE: ', obj)
         }
     });
     // For every string in the object
