22f0b824d59a0c2910ab7e524f1a47fd5715ac86
Jack Roybal <118242834+ExcuseMeImJack@users.noreply.github.com>
2023-07-03 10:49:01 -0700

    Time: 136 ms (26.34%), Space: 42.6 MB (56.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,14 +4,24 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let output = [];
-    const length = nums.length
-    for (let i = 0; i < length; i++) {
-        for (let j = i + 1; j < length; j++) {
-            if (nums[i] + nums[j] === target) {
-                output.push(i, j) 
+    // Input: Array of nums and a target
+    // Return: Indices of the two numbers that add up to the target
+    
+    // Create an empty output array.
+    const output = [];
+    // Get the length of the nums array.
+    const length = nums.length;
+    
+    // Iterate twice (i & j) one for the current number and one for the next number.
+    for(let i = 0; i < length; i++){
+        for(let j = i + 1; j < length; j++){
+            // Check to see if the values add up to the target.
+            if(nums[i] + nums[j] === target){
+                // If they do, push them to the output array.
+                output.push(i, j)
             }
         }
     }
-    return output
+        
+    return output;
 };
